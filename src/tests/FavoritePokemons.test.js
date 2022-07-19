fd5166173e9077b418b1b221d86ddba063f6188b
Willian Felipe <w.felipebraz@gmail.com>
2022-07-19 17:37:21 -0300

    questão 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test(`se é exibida na tela a mensagem No favorite 
+pokemon found, caso a pessoa não tenha pokémons favoritos`, () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/favorites');
+  const notFound = screen.getByText(/no favorite pokemon found/i);
+  expect(notFound).toBeInTheDocument();
+});
