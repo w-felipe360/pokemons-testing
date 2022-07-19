380c33f35ff463b6a8197f94ac0af1d6ad242d81
Willian Felipe <w.felipebraz@gmail.com>
2022-07-19 17:48:12 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test(`se a página contém um heading h2 com o texto Page 
+requested not found 😭`, () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/*');
+  const notFound = screen.getByRole('heading', {
+    name: /page requested not/i,
+  });
+  expect(notFound);
+});
+test(`Teste se a página mostra a imagem 
+  https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/*');
+  const pikachu = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  expect(pikachu).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
